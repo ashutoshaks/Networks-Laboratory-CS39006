d3b5397715047301289ad44082c6333754715b09
ashutoshaks <ashu11.03.2002@gmail.com>
2022-01-24 12:14:53 +0530

    Go back to previous method

--- Assignment-2/19CS30008_Assgn2b/newdnsclient.c
+++ Assignment-2/19CS30008_Assgn2b/newdnsclient.c
@@ -48,43 +48,10 @@ int main()
         exit(1);
     }
 
-    // Make the socket non-blocking
-    int args = fcntl(sockfd, F_GETFL);
-    args |= O_NONBLOCK;
-    fcntl(sockfd, F_SETFL, args);
-
-    int ret = connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-    if(ret < 0) {
-        if(errno == EINPROGRESS) {
-            struct timeval tv;
-            tv.tv_sec = 2;
-            tv.tv_usec = 0;
-            fd_set fd;
-            FD_ZERO(&fd);
-            FD_SET(sockfd, &fd);
-            ret = select(sockfd + 1, NULL, &fd, NULL, &tv);
-            if(ret > 0) {
-                socklen_t len = sizeof(int);
-                getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &ret, (socklen_t *) &len);
-                if(ret != 0) {
-                    perror("Unable to connect\n");
-                    exit(1);
-                }
-            }
-            else {
-                printf("\nTimed out in 2 sec - No response from server\n");
-                exit(1);
-            }
-        }
-        else {
-            perror("Unable to connect\n");
-            exit(1);
-        }
+    if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Unable to connect to server\n");
+        exit(1);
     }
-
-    // Make the socket blocking again
-    args &= (~O_NONBLOCK);
-    fcntl(sockfd, F_SETFL, args);
 
     send(sockfd, buf, strlen(buf) + 1, 0);
 
